59a9711b893997b8d22486ed968690fcd831f802
Adrien PAYEN <adrien.payen2@gmail.com>
2018-06-13 17:46:46 +0200

    update auth to add role field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_104959) do
+ActiveRecord::Schema.define(version: 2018_06_12_213252) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_06_12_104959) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 ActiveRecord::Schema.define(version: 2018_06_12_105005) do
