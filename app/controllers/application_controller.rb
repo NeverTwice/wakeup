d0592bb9d44dd0243bc0c0fb70203bc96c39932b
sam <samuelantunes@hotmail.fr>
2018-06-27 14:31:02 +0200

    User redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,12 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
 
   def after_sign_in_path_for resource
-    if resource.is_a?(User)
-      # Redirections for bakers
-      bakery = Bakery.where(:user_id => resource.id).first
-      if bakery
-        bakery
-      else
-        "/bakeries/new"
-      end
-    else
-      super
-    end
+    user_redirection resource
+  end
+
+  def after_sign_up_path_for resource
+    user_redirection resource
   end
 
 end
